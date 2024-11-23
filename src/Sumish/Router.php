79db0975570d6dc4dcd5481e3423657cd6bada86
vsko <vsko@anticloud.info>
2024-11-23 19:42:00 +0000

    Update Router with new realisation

--- src/Sumish/Router.php
+++ src/Sumish/Router.php
@@ -8,8 +8,10 @@
 
 namespace Sumish;
 
+use Sumish\Exceptions\NotFoundException;
+
 /**
- * Класс Router отвечает за маршрутизацию запросов в приложении.
+ * Класс Router отвечает за маршрутизацию запросов.
  *
  * Он управляет загрузкой маршрутов, сопоставлением URI с контроллерами и
  * обработкой входящих запросов.
@@ -18,50 +20,14 @@ namespace Sumish;
  */
 class Router {
     /**
-     * Контейнер зависимостей.
-     *
-     * @var \Sumish\Container
-     */
-    private Container $container;
-
-    /**
      * Массив маршрутов.
      *
      * @var array
      */
     private $routes = [];
 
-    /**
-     * Конструктор класса Router.
-     *
-     * @param Container $container Контейнер зависимостей.
-     */
-    function __construct(Container $container) {
+    function __construct(private Container $container) {
         $this->container = $container;
-    }
-
-    /**
-     * Получает URI из запроса.
-     *
-     * Проверяет наличие параметра 'route' в GET-запросе и извлекает URI из
-     * текущего запроса, обрабатывая базовый путь.
-     *
-     * @return string Возвращает обработанный URI.
-     */
-    public function getURI() {
-        if (isset($_GET['route']) && !empty($_GET['route'])) {
-            $route = $_GET['route'];
-        } else {
-            $route = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    
-            $basePath = dirname($_SERVER['SCRIPT_NAME']);
-            if (strpos($route, $basePath) === 0) {
-                $route = substr($route, strlen($basePath));
-            }
-        }
-    
-        $route = '/' . trim($route, '/');
-        return $route ?: '/';
     }
 
     /**
@@ -71,7 +37,7 @@ class Router {
      * @return array Возвращает объединённый массив маршрутов.
      */
     public function configureRoutes(array $routes = []) {
-        foreach (glob(DIR_APP .'/*/config.php') as $config) {
+        foreach (glob(getcwd() . '/app/*/config.php') as $config) {
             $config = include($config);
             if ($config['routes']) {
                 $routes = array_merge($routes, $config['routes']);
@@ -79,6 +45,20 @@ class Router {
         }
 
         return $routes;
+    }
+
+    /**
+     * Получает маршрут по его имени.
+     *
+     * @param string|null $route Имя маршрута.
+     * @return array Возвращает массив маршрута или null, если не найдено.
+     */
+    public function get($route = null): array {
+        if (is_null($route)) {
+            return $this->routes;
+        } else {
+            return [$route, $this->routes[$route]];
+        }
     }
 
     /**
@@ -94,8 +74,8 @@ class Router {
         }
 
         if (is_array($route) && is_null($target)) {
-            $target = $route[1];
             $route = $route[0];
+            $target = $route[1];
         }
 
         $this->routes[$route] = $target;
@@ -110,6 +90,8 @@ class Router {
      * @return $this Возвращает текущий экземпляр Router.
      */
     public function push(array $routes) {
+        // $routes = $this->configureRoutes($routes);
+        
         foreach ($routes as $route => $target) {
             $this->add($route, $target);
         }
@@ -117,116 +99,84 @@ class Router {
         return $this;
     }
 
-    /**
-     * Загружает маршруты и выполняет их сопоставление с текущим URI.
-     *
-     * @param array $routes Массив маршрутов для загрузки.
-     */
-    public function load(array $routes = []) {
-        $uri = $this->getURI();
-        $routes = $this->configureRoutes($routes);
-
-        $this->push($routes)->match($uri);
-    }
-
-    /**
-     * Получает маршрут по его имени.
-     *
-     * @param string|null $route Имя маршрута.
-     * @return array|null Возвращает массив маршрута или null, если не найдено.
-     */
-    public function get($route = null) {
-        if (is_null($route)) {
-            return $this->routes;
-        } else {
-            return [$route, $this->routes[$route]];
-        }
-    }
-
-    /**
-     * Сопоставляет текущий URI с зарегистрированными маршрутами.
-     *
-     * @param string $uri URI для сопоставления.
-     * @return bool Возвращает true, если сопоставление успешно, иначе false.
-     */
-    public function match($uri) {
-        $params = $parts = [];
-        $app = new class {
-            public $name;
-            public $controller;
-            public $action;
-            public $params;
-            public $path;
-        };
-
+    public function match(string $uri): array {
         foreach ($this->routes as $route => $target) {
-            if (preg_match("#^{$route}$#i", $uri, $params)) {
-                $parts = explode('/', $target);
-
-                $app->name = strtolower($parts[0]);
-                $app->controller = ucfirst($parts[0]) . 'Controller';
-                $app->action = (count($parts) == 2) ? $parts[1] . 'Action'
-                    : ((count($parts) == 1) ? 'indexAction' : null);
-
-                if (is_null($app->action)) {
-                    trigger_error('No match action');
-                }
-
-                $app->params = $params;
-                $app->path = DIR_APP . '/' . $app->name;
-                break;
+            $pattern = preg_replace('#\{(\w+)\}#', '(?P<$1>[^/]+)', $route);
+            $pattern = "#^{$pattern}$#";
+    
+            if ($route === $uri || preg_match($pattern, $uri, $matches)) {
+                return [
+                    'controller' => $target['controller'],
+                    'action' => $target['action'],
+                    'parameters' => isset($matches) ? array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY) : [],
+                ];
             }
         }
 
-        return $this->execute($app);
+        throw new NotFoundException("Route not found for URI: {$uri}", 404);
     }
 
     /**
-     * Выполняет контроллер с заданным приложением.
+     * Находит и создаёт экземпляр контроллера на основе маршрута.
      *
-     * @param object $app Объект приложения с данными контроллера и действия.
-     * @param bool $register Указывает, нужно ли регистрировать контроллер.
-     * @return bool Возвращает true, если выполнение успешно, иначе false.
+     * @param array $match Данные маршрута: имя контроллера и действие.
+     * @return object Экземпляр контроллера.
+     * @throws Exception Если файл контроллера или класс не найден.
      */
-    public function execute($app, $register = true) {
-        $ownController = ucfirst($app->name) . ucfirst(str_replace('action', '', strtolower($app->action))) . 'Controller';
-
-        $pathFiles = [
-            '/controller.php',
-            '/controllers/' . $app->controller . '.php',
-            '/controllers/' . $ownController . '.php'
-        ];
-
-        foreach ($pathFiles as $file) {
-            $file = $app->path . $file;
-
-            if (is_file($file)) {
-                require_once($file);
-
-                if (class_exists($app->controller, false)) {
-                    if (method_exists($app->controller, $app->action)) {
-                        break;
-                    }
-                }
-
-                if (class_exists($ownController, false)) {
-                    $app->controller = $ownController;
-                    $app->action = 'indexAction';
-                    break;
-                }
-            }
+    public function resolveController(array $match): object {
+        if (!isset($match['controller'])) {
+            throw new \Exception("No controller defined in route.");
         }
 
-        if (method_exists($app->controller, $app->action)) {
-            if ($register) {
-                $this->container->register('app', $app);
-                $this->container->register('controller', $app->controller);
-                return true;
-            } else {
-                return $this->container->build($app->controller);
-            }
-        } else {
-            return false;
+        if (!isset($match['action'])) {
+            throw new \Exception("No action defined in route.");
         }
+
+        $controller = $match['controller'];
+        $controllerClass = "App\\Controllers\\{$controller}";
+        $controllerPath = getcwd() . "/app/controllers/{$controller}.php";
+
+        if (!is_file($controllerPath)) {
+            throw new \Exception("Controller file not found: {$controllerPath}");
+        }
+
+        require_once $controllerPath;
+
+        if (!class_exists($controllerClass)) {
+            throw new \Exception("Controller class not found: {$controllerClass}");
+        }
+
+        $controllerInstance = new $controllerClass($this->container);
+        $controllerInstance->match = $match;
+
+        return $controllerInstance;
+    }
+
+    /**
+     * Выполняет указанное действие контроллера.
+     *
+     * Метод вызывает действие контроллера, передавая параметры маршрута.
+     *
+     * @param object $controller Экземпляр контроллера.
+     * @return mixed Результат выполнения действия контроллера.
+     * @throws Exception Если действие не определено или отсутствует.
+     */
+    public function dispatch(object $controller) {
+        $action = $controller->match['action'];
+        $parameters = $controller->match['parameters'] ?? [];
+
+        if (!method_exists($controller, $action)) {
+            throw new \Exception("Action method '{$action}' not found in controller " . get_class($controller));
+        }
+    
+        $reflection = new \ReflectionMethod($controller, $action);
+        $methodParameters = $reflection->getParameters();
+
+        $parameters = array_map(function ($parameter) use ($parameters) {
+            $name = $parameter->getName();
+            return $parameters[$name] ?? ($parameter->isDefaultValueAvailable() ? $parameter->getDefaultValue() : null);
+        }, $methodParameters);
+    
+        return call_user_func_array([$controller, $action], $parameters);
     }
 }
