ec00227557e239c3ffdeba0c635b8476cad76ef5
vsko <vsko@anticloud.info>
2024-11-23 19:25:22 +0000

    Update Application with new realisation

--- src/Sumish/Application.php
+++ src/Sumish/Application.php
@@ -8,6 +8,8 @@
 
 namespace Sumish;
 
+use Sumish\Exceptions\HandlerException;
+
 /**
  * Главный класс для инициализации и управления приложением.
  *
@@ -15,13 +17,12 @@ namespace Sumish;
  * а также управляет ошибками и отвечает за запуск контроллеров.
  *
  * @package Sumish
- * @version 1.0.1
  */
 class Application {
     /**
      * Контейнер зависимостей.
      *
-     * @var \Sumish\Container
+     * @var Container
      */
     private Container $container;
 
@@ -30,7 +31,7 @@ class Application {
      *
      * @var array
      */
-    private array $config;
+    private array $config = [];
 
     /**
      * Конструктор класса Application.
@@ -38,165 +39,92 @@ class Application {
      * @param array $config Массив конфигурации приложения.
      */
     public function __construct(array $config = []) {
-        $this->config = $this->configure($config);
-
-        $this->initializeContainer();
-        $this->initializeRoutes();
-        $this->initializeLibraries();
-
-        $this->setupResponse();
+        $this->configure($config);
+        $this->container = Container::create($this->config['components']);
+        $this->container->register('config', $this->config);
+        // $this->container->load->libraries($this->config['libraries'] ?? []);
     }
 
     /**
-     * Конфигурирует приложение, объединяя значения по умолчанию и пользовательские настройки.
+     * Конфигурирует приложение, объединяя значения по умолчанию.
      *
      * @param array $config Массив конфигурации.
      * @return array Объединённый массив конфигурации.
      */
-    private function configure(array $config): array {
-        return array_replace_recursive(self::configDefault(), $config);
+    public function configure(array $config): array {
+        $this->config = array_replace_recursive(self::configDefault(), $this->config, $config);
+        return $this->config;
     }
 
     /**
-     * Инициализирует контейнер зависимостей приложения.
+     * Получает контейнер зависимостей.
      *
-     * Этот метод создает контейнер и регистрирует в нем 
-     * конфигурацию приложения.
-     *
-     * @return void
+     * @return Container Объект контейнера зависимостей.
      */
-    private function initializeContainer() {
-        $components = $this->config['components'] ?? [];
-        $this->container = Container::create(array_merge(self::componentsDefault(), $components));
-        $this->container->register('config', $this->config);
-    }
-
-    public function container() {
+    public function container(): Container {
         return $this->container;
     }
 
     /**
-     * Инициализирует маршруты приложения.
-     *
-     * Этот метод загружает маршруты из конфигурации 
-     * и устанавливает их в контейнере маршрутов.
-     *
-     * @return void
-     */
-    private function initializeRoutes() {
-        $routes = $this->config['routes'] ?? [];
-        $this->container->route->load($routes);
-    }
-
-    /**
-     * Инициализирует библиотеки приложения.
-     *
-     * Этот метод загружает библиотеки из конфигурации 
-     * и устанавливает их в контейнере.
-     *
-     * @return void
-     */
-    private function initializeLibraries() {
-        $libraries = $this->config['libraries'] ?? [];
-        $this->container->load->libraries($libraries);
-    }
-
-    /**
-     * Настраивает объект ответа приложения.
-     *
-     * Этот метод добавляет заголовки и устанавливает уровень сжатия 
-     * для ответа на основе конфигурации приложения.
-     *
-     * @return void
-     */
-    private function setupResponse() {
-        $this->container->response->addHeaders($this->config['headers'] ?? []);
-        $this->container->response->setCompression($this->config['compression'] ?? 0);
-    }
-
-    /**
-     * Запускает приложение.
-     *
-     * Этот метод проверяет, установлен ли контроллер, и выполняет 
-     * его метод dispatch для обработки запроса. Если контроллер 
-     * не установлен, вызывается метод ошибки с кодом 404. 
-     * Затем инициализируется объект ответа.
+     * Запуск приложения.
      *
      * @return void
      */
     public function run() {
-        $controller = $this->container->controller;
+        HandlerException::register();
 
-        if ($controller) {
-            $controller->dispatch();
-        } else {
-            $this->error(404);
+        $router = $this->container->router;
+        $request = $this->container->request;
+        $response = $this->container->response;
+
+        $uri = $request->getUri();
+        
+        $response->addHeaders($this->config['headers'] ?? []);
+        $response->setCompression($this->config['compression'] ?? 0);
+
+        $matched = $router->push($this->config['routes'])->match($uri);
+        $controller = $router->resolveController($matched);
+
+        try {
+            $data = $router->dispatch($controller);
+            $response->setOutput($data);
+            $response->init();
+        } catch(\Throwable $exception) {
+            HandlerException::handleException($exception);
         }
-
-        $this->container->response->init();
     }
 
     /**
-     * Обрабатывает ошибки приложения.
-     *
-     * Этот метод устанавливает сообщение об ошибке в зависимости 
-     * от переданного кода состояния и выводит его через объект ответа.
-     *
-     * @param int $status Код состояния ошибки.
-     * @return void
-     */
-    private function error($status) {
-        $errorText = 'Error ';
-    
-        switch ($status) {
-            case 403:
-                $errorText .= '403 Forbidden';
-                break;
-            case 404:
-                $errorText .= '404 Not Found';
-                break;
-            case 500:
-                $errorText .= '500 Internal Server Error';
-                break;
-            default:
-                $errorText .= 'Unknown Error';
-                break;
-        }
-    
-        $this->container->response->setOutput($errorText);
-    }
-
-    /**
-     * Возвращает массив компонентов по умолчанию для контейнера.
+     * Возвращает массив компонентов по умолчанию.
      *
      * Этот метод предоставляет стандартный набор компонентов, 
      * которые могут быть использованы приложением.
      *
-     * @return array Возвращает ассоциативный массив компонентов.
+     * @return array Возвращает массив компонентов.
      */
     public static function componentsDefault(): array {
         return [
-            'load' => \Sumish\Loader::class,
-            'route' => \Sumish\Router::class,
-            'session' => \Sumish\Session::class,
+            'loader' => \Sumish\Loader::class,
+            'router' => \Sumish\Router::class,
             'request' => \Sumish\Request::class,
             'response' => \Sumish\Response::class,
+            'session' => \Sumish\Session::class,
             'view' => \Sumish\View::class,
         ];
     }
 
     /**
-     * Возвращает массив конфигурации по умолчанию для приложения.
+     * Возвращает массив конфигурации по умолчанию.
      *
      * Этот метод предоставляет стандартный набор конфигурационных 
      * параметров, которые могут быть использованы приложением 
      * при его инициализации.
      *
-     * @return array Возвращает ассоциативный массив конфигурации.
+     * @return array Возвращает массив конфигурации.
      */
     public static function configDefault(): array {
         return [
-            'routes' => ['/' => 'home'],
+            'routes' => ['/' => ['controller' => 'MainController', 'action' => 'index']],
             'headers' => [1000 => 'Content-Type: text/html; charset=utf-8'],
             'components' => self::componentsDefault(),
             'libraries' => [],
